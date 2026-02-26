bb9e0f11d914f08f741c68e7ed4d92f6941d155c
ERIC DE LUCAS SILVA <ericdelucass@gmail.com>
2026-02-25 22:30:57 -0300

    fdg

---
+++ vetores/src/aula2/Carro.java
@@ -0,0 +1,20 @@
+package aula2;
+public class Carro {
+
+
+        // TODO Auto-generated method stub
+        String marca;
+        int ano;
+        String modelo;
+
+        //construtor padrao
+        Carro(){
+            modelo = "Desconhecido";
+        }
+        Carro(String m){
+            this.marca = m;
+
+
+    }
+
+}
---
+++ vetores/src/aula2/TesteCarro.java
@@ -0,0 +1,14 @@
+package aula2;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Carro c1 = new Carro();
+        c1.marca = "Fiat";
+        System.out.println("Modelo:"+c1.marca);
+        System.out.println(c1.modelo);
+
+        Carro c2 = new Carro("Peugeout");
+        System.out.println(c2.marca);
+    }
+}
